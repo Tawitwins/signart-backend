03b3fd13ffa6c0deedf62a842132da3770301d63
Lamine Syne (Stagiaire Développeur) <snsynel@Modelsis.net>
2019-12-18 13:26:34 +0000

    adresse change to lieu

--- src/main/java/sn/modelsis/signart/Formation.java
+++ src/main/java/sn/modelsis/signart/Formation.java
@@ -59,10 +59,10 @@ public class Formation implements Serializable {
         this.id = id;
     }
 
-    public Formation(Integer id, String adresse,String libelle,Integer anneeDebut,Integer anneeFin,String specialisation) {
+    public Formation(Integer id, String lieu,String libelle,Integer anneeDebut,Integer anneeFin,String specialisation) {
         this.id = id;
         this.libelle=libelle;
-        this.lieu = adresse;
+        this.lieu = lieu;
         this.anneeDebut=anneeDebut;
         this.anneeFin=anneeFin;
         this.specialisation=specialisation;
