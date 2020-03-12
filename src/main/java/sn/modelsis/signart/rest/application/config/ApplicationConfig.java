3ef737f8f7bf5aaade409bb4137de8cecba114bf
snmbengueo <ombengue-stagiaire@modelsis.sn>
2020-03-12 16:44:59 +0000

    Ajout des api pour la table visiteur créer et mise a jour des api de la table marquageArtiste

--- src/main/java/sn/modelsis/signart/rest/application/config/ApplicationConfig.java
+++ src/main/java/sn/modelsis/signart/rest/application/config/ApplicationConfig.java
@@ -81,5 +81,6 @@ public class ApplicationConfig extends Application {
         resources.add(sn.modelsis.signart.service.TypeCommandeFacadeREST.class);
         resources.add(sn.modelsis.signart.service.TypeCompteFacadeREST.class);
         resources.add(sn.modelsis.signart.service.UserFacadeREST.class);
+        resources.add(sn.modelsis.signart.service.VisiteurFacadeREST.class);
     }
 }
