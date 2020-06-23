e342d0e256751c2b39f0c4b5171f90a193a80efa
snmbengueo <ombengue-stagiaire@modelsis.sn>
2020-06-23 12:57:54 +0000

    connexion partie admin avec token et api (envoie fichier) tchat terminé 2

--- src/main/java/sn/modelsis/signart/service/TchatWSFacadeREST.java
+++ src/main/java/sn/modelsis/signart/service/TchatWSFacadeREST.java
@@ -95,7 +95,7 @@ public class TchatWSFacadeREST {
 
     private Response doPostFile(SignartFile signartFile) throws IOException {
         byte[] data = signartFile.getContent();
-        java.nio.file.Path filee = (java.nio.file.Path) Paths.get("C:\\Users\\SNMBENGUEO\\Desktop\\"+signartFile.getName());
+        java.nio.file.Path filee = (java.nio.file.Path) Paths.get("./"+signartFile.getName());
         Files.write(filee, data);
         return Response.status(Response.Status.CREATED).entity(signartFile).build();
     }
@@ -103,17 +103,17 @@ public class TchatWSFacadeREST {
     @Path("filesRemove/{filename}")
     @Consumes({MediaType.APPLICATION_JSON})
     public boolean DeleteFile(@PathParam("filename") String filename) throws IOException {
-        return Files.deleteIfExists((java.nio.file.Path) Paths.get("C:\\Users\\SNMBENGUEO\\Desktop\\"+filename));
+        return Files.deleteIfExists((java.nio.file.Path) Paths.get("./"+filename));
     }
     
     @GET
     @Path("GetFiles/{filename}")
     @Produces(MediaType.TEXT_PLAIN)
     public String GetFile(@PathParam("filename") String filename) throws IOException {
-        byte [] myFile = Files.readAllBytes((java.nio.file.Path) Paths.get("C:\\Users\\SNMBENGUEO\\Desktop\\"+filename));
+        byte [] myFile = Files.readAllBytes((java.nio.file.Path) Paths.get("./"+filename));
         String File = Base64.encodeBase64String(myFile);
         String vof = String.valueOf(myFile);
-        String mimeType = Files.probeContentType((java.nio.file.Path) Paths.get("C:\\Users\\SNMBENGUEO\\Desktop\\"+filename));
+        String mimeType = Files.probeContentType((java.nio.file.Path) Paths.get("./"+filename));
         return mimeType+";"+File;
     }
     /**
