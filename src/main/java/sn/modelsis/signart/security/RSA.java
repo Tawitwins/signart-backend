ace85aee8929383a237573774bef835c1335f61d
Khaly DIENG <diengkhaly17@gmail.com>
2022-12-15 15:13:53 +0000

    encryption/decryption with RSA algorithm

---
+++ src/main/java/sn/modelsis/signart/security/RSA.java
@@ -0,0 +1,80 @@
+package sn.modelsis.signart.security;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class RSA {
+    private PrivateKey privatekey;
+    private PublicKey publicKey;
+
+    public RSA(){
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(2048);
+            KeyPair pair = generator.generateKeyPair();
+            privatekey = pair.getPrivate();
+            publicKey = pair.getPublic();
+        } catch (Exception ignored) {}
+    }
+
+    public String encode(byte[] data){
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    private byte[] decode(String data){
+        return Base64.getDecoder().decode(data);
+    }
+
+    public static PrivateKey getPrivateKey(String base64PrivateKey){
+        PrivateKey privateKey = null;
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(base64PrivateKey.getBytes()));
+        KeyFactory keyFactory = null;
+        try {
+            keyFactory = KeyFactory.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        try {
+            privateKey = keyFactory.generatePrivate(keySpec);
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return privateKey;
+    }
+
+    public static PublicKey getPublicKey(String base64PrivateKey){
+        PublicKey publicKey = null;
+        try{
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PrivateKey.getBytes()));
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            publicKey = keyFactory.generatePublic(keySpec);
+            return publicKey;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return publicKey;
+    }
+
+    public static byte[] encrypt(String data, String keypub) throws BadPaddingException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, getPublicKey(keypub));
+        return cipher.doFinal(data.getBytes());
+    }
+
+    public String decrypt(String encryptedMessage, String KeyPrivate) throws Exception{
+        byte[] encryptedBytes = decode(encryptedMessage);
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE,getPrivateKey(KeyPrivate));
+        byte[] decryptedMessage = cipher.doFinal(encryptedBytes);
+        return new String(decryptedMessage,"UTF8");
+    }
+}
---
+++ src/main/java/sn/modelsis/signart/security/RsaRest.java
@@ -0,0 +1,46 @@
+package sn.modelsis.signart.security;
+
+import sn.modelsis.signart.Parametrage;
+import sn.modelsis.signart.facade.ParametrageFacade;
+import sn.modelsis.signart.facade.UtilisateurFacade;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("security")
+public class RsaRest {
+    @Inject
+    ParametrageFacade parametrageFacade;
+    RSA rsa = new RSA();
+    @POST
+    @Path("encrypt")
+    @Consumes({MediaType.TEXT_PLAIN})
+    public String encrypt(String data) throws Exception{
+        String cipher = null;
+        Parametrage param = parametrageFacade.findByParamName("RSA_PUBLIC_KEY");
+        if(param != null) {
+            byte[] dd = rsa.encrypt(data,param.getValue());
+            cipher = rsa.encode(dd);
+        }
+       return cipher;
+    }
+
+    @POST
+    @Path("decrypt")
+    @Consumes({MediaType.TEXT_PLAIN})
+    public String decrypt(String cipherText)
+            throws Exception {
+        String plainText = null;
+        Parametrage param = parametrageFacade.findByParamName("RSA_PRIVATE_KEY");
+        if(param != null) {
+            plainText = rsa.decrypt(cipherText,param.getValue());
+        }
+        return plainText;
+    }
+}
